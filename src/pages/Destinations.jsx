23a2219668a0c62f7b3d6abd54a47d8f4913d740
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-06-09 12:35:22 +0530

    cvvc

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -3,6 +3,18 @@ import api from "../services/api";
 import DestinationCard from "../components/DestinationCard";
 import { Link } from "react-router-dom";
 function Destinations() {
+  const [search, setSearch] = useState("");
+  const [category, setCategory] =
+  useState("All");
+
+  const [budget, setBudget] =
+  useState("All");
+
+  const [sort, setSort] =
+  useState("");
+
+
+
  const [destinations, setDestinations] =
    useState([]);
 
@@ -35,6 +47,55 @@ async function deleteDestination(id) {
  );
 }
 
+const filteredDestinations =
+  destinations.filter(
+    destination => {
+
+      const searchMatch =
+        destination.name
+          .toLowerCase()
+          .includes(
+            search.toLowerCase()
+          );
+
+      const categoryMatch =
+        category === "All" ||
+        destination.category ===
+          category;
+const budgetMatch =
+  budget === "All" ||
+  destination.budget === budget;
+
+      return (
+        searchMatch &&
+        categoryMatch &&
+  budgetMatch
+
+      );
+    }
+  );
+
+  let finalDestinations =
+  [...filteredDestinations];
+
+if (sort === "high") {
+
+  finalDestinations.sort(
+    (a, b) =>
+      b.rating - a.rating
+  );
+
+}
+
+if (sort === "low") {
+
+  finalDestinations.sort(
+    (a, b) =>
+      a.rating - b.rating
+  );
+
+}
+
  return (
    <>
 
@@ -42,9 +103,64 @@ async function deleteDestination(id) {
    <Link to="/add-destination" className='submit-btn'>
  Add Destination
 </Link>
+<div className="filters">
+  <input
+  type="text"
+  placeholder="Search Destination"
+  value={search}
+  onChange={(e) =>
+    setSearch(e.target.value)
+  }
+/>
+
+<select
+  value={category}
+  onChange={(e) =>
+    setCategory(e.target.value)
+  }
+>
+  <option>All</option>
+  <option>Beach</option>
+  <option>Hill Station</option>
+  <option>Adventure</option>
+</select>
+
+<select
+  value={budget}
+  onChange={(e) =>
+    setBudget(e.target.value)
+  }
+>
+  <option>All</option>
+  <option>Low</option>
+  <option>Medium</option>
+  <option>High</option>
+</select>
+
+
+<select
+  value={sort}
+  onChange={(e) =>
+    setSort(e.target.value)
+  }
+>
+  <option value="">
+    Select
+  </option>
+
+  <option value="high">
+    High To Low
+  </option>
+
+  <option value="low">
+    Low To High
+  </option>
+</select>
+
+</div>
 
      <div className="destinations">
-       {destinations.map((destination) => (
+       {finalDestinations.map((destination) => (
          <DestinationCard
            key={destination.id}
            destination={destination}
